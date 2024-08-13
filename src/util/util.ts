f91dfb5aa79fb3b082ebb72d249fcaaeb25fc027
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-08-13 08:25:05 -0300

    Create util.ts

---
+++ src/util/util.ts
@@ -0,0 +1,23 @@
+import { $isAtNodeEnd } from "@lexical/selection";
+import { ElementNode, RangeSelection, TextNode } from "lexical";
+
+export function getSelectedNode(
+  selection: RangeSelection
+): TextNode | ElementNode {
+  const anchor = selection.anchor;
+  const focus = selection.focus;
+  const anchorNode = selection.anchor.getNode();
+  const focusNode = selection.focus.getNode();
+  if (anchorNode === focusNode) {
+    return anchorNode;
+  }
+  const isBackward = selection.isBackward();
+  if (isBackward) {
+    return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+  } else {
+    return $isAtNodeEnd(anchor) ? anchorNode : focusNode;
+  }
+}
+
+export const emojiRegExp =
+  /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g;
