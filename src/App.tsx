7817db48c976c101f94a78a3ddcdb9987c96e168
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-08-13 08:24:34 -0300

    remove and install

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import RichTextCard from "./RichTextCard";
+import { Card, Stack } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [text, setText] = useState<string>("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Card sx={{ width: "100%", alignContent: "center" }} variant="outlined">
+      <h2>Play with text</h2>
+      <Stack maxWidth="80%" alignItems="center">
+        <RichTextCard html={text} setHTML={(value) => setText(value)} />;
+      </Stack>
+    </Card>
+  );
+};
 
-export default App
+export default App;
